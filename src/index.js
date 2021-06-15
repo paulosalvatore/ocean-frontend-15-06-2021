f6c4798fb3ce44468da3c3cd157d48eb9f4a7b7a
Paulo Salvatore <salvatoregames@gmail.com>
2021-06-15 11:30:52 -0300

    Criação da estrutura de componentes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+function CardItem() {
+    return <div>Item</div>;
+}
+
+function ListarItens() {
+    return (
+        <div>
+            <CardItem />
+            <CardItem />
+        </div>
+    );
+}
+
+function App() {
+    return <ListarItens />;
+}
+
 ReactDOM.render(
     <React.StrictMode>
-        <div>Hello, world!</div>
+        <App />
     </React.StrictMode>,
     document.getElementById("root")
 );
