8a89f62301ddff7723c19264b891ce554465387d
Paulo Salvatore <salvatoregames@gmail.com>
2021-06-15 12:24:36 -0300

    Bilbo adicionado

--- src/index.js
+++ src/index.js
@@ -18,9 +18,15 @@ function ListarItens() {
                 nome="Golum"
                 imagem="https://img.olhardigital.com.br/wp-content/uploads/2019/11/20191119040151-860x450.jpg"
             />
+
             <CardItem
                 nome="Frodo"
                 imagem="http://pm1.narvii.com/6308/61e613bfde1b95f18e14afda9fa97215cd2ee6e4_00.jpg"
+            />
+
+            <CardItem
+                nome="Bilbo"
+                imagem="https://cdn.vox-cdn.com/thumbor/OnqCOd14_2wyCsgOo5FQe4hJNns=/0x0:3733x1579/1200x675/filters:focal(1499x515:2095x1111)/cdn.vox-cdn.com/uploads/chorus_image/image/68661007/Lord_of_the_Rings_The_Fellowship_of_the_Ring.mkv_snapshot_00.19.38__2020.11.29_18.16.35_.0.png"
             />
         </div>
     );
