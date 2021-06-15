52d046f7eb8144bfb84e8e1af27fcb8b8e013846
Paulo Salvatore <salvatoregames@gmail.com>
2021-06-15 11:04:03 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <div>Hello, world!</div>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
