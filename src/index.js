624c2072c226299326bfd3d206a898d9e79315bd
Paulo Salvatore <salvatoregames@gmail.com>
2021-06-16 12:14:00 -0300

    Substituição da lista de dados pelo acesso ao backend

--- src/index.js
+++ src/index.js
@@ -2,27 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const itens = [
-    {
-        nome: "Bilbo",
-        imagem: "https://cdn.vox-cdn.com/thumbor/OnqCOd14_2wyCsgOo5FQe4hJNns=/0x0:3733x1579/1200x675/filters:focal(1499x515:2095x1111)/cdn.vox-cdn.com/uploads/chorus_image/image/68661007/Lord_of_the_Rings_The_Fellowship_of_the_Ring.mkv_snapshot_00.19.38__2020.11.29_18.16.35_.0.png",
-    },
-    {
-        nome: "Golum",
-        imagem: "https://img.olhardigital.com.br/wp-content/uploads/2019/11/20191119040151-860x450.jpg",
-    },
-    {
-        nome: "Frodo",
-        imagem: "http://pm1.narvii.com/6308/61e613bfde1b95f18e14afda9fa97215cd2ee6e4_00.jpg",
-    },
-    {
-        nome: "Gandalf",
-        imagem: "/assets/Gandalf.jpg",
-    },
-];
-
-console.log(JSON.stringify(itens));
-
 function CardItem(props) {
     return (
         <div className="card_item">
@@ -33,20 +12,39 @@ function CardItem(props) {
 }
 
 class ListarItens extends React.Component {
+    // 1 - Definir estado inicial
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            itens: [],
+        };
+    }
+
     async componentDidMount() {
         console.log("Componente ListarItens construído.");
 
-        const request = await fetch("https://pokeapi.co/api/v2/pokemon");
+        const request = await fetch("https://backend-flexivel.herokuapp.com/", {
+            headers: new Headers({
+                Authorization: "profpaulo.salvatore",
+            }),
+        });
 
         const json = await request.json();
+
+        // 2 - Atualiza o estado
+        this.setState({
+            itens: json,
+        });
 
         console.log(json);
     }
 
     render() {
+        // 3 - Renderiza utilizando a informação que está no estado
         return (
             <div className="lista_itens">
-                {itens.map((item, index) => (
+                {this.state.itens.map((item, index) => (
                     <CardItem item={item} key={index} />
                 ))}
             </div>
